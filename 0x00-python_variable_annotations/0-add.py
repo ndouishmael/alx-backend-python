610a5574b24f88ef2622afc65dd05af99938535d
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-07-06 17:09:49 +0200

    Create 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two floats.
+
+    Args:
+        a: The first float.
+        b: The second float.
+
+    Returns:
+        The sum of `a` and `b`.
+    """
+    return a + b
