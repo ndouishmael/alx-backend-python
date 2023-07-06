34e4a2a46c7fbe314be651d1576dcb91aa9f956c
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-07-06 17:21:52 +0200

    Update 0-add.py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 
+'''
+    Basic annotations for variables.
+'''
+
+
 def add(a: float, b: float) -> float:
-    """
-    Returns the sum of two floats.
-
-    Args:
-        a: The first float.
-        b: The second float.
-
-    Returns:
-        The sum of `a` and `b`.
-    """
+    '''
+        Returns sum of two floats.
+    '''
     return a + b
