bf646d396a0c80df7df0acafa07aebf86cba2ce4
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-07-07 22:57:37 +0200

    Delete 6-sum_mixed_list.py

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-""" mixed list """
-from typing import Union, List
-
-
-def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    """ Returns the sum of integers and floats as a float. """
-    return float(sum(mxd_lst))
